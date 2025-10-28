5753d46731cb057bdda25cc413413c81dbb3bd8e
Jyoti Ranjan Biwal <jranjan2017@gmail.com>
2025-10-28 10:52:11 +0530

    chore: removed redirection

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  redirects: async () => {
-    return [
-      {
-        source: '/',
-        destination: 'https://ecom-cart-be-dphn.vercel.app/',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
